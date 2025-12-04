fb0cc9f04ed8f08e82758185c3513c70c27697f2
wahyusuhar <wahyuwakteng@gmail.com>
2025-12-05 02:27:42 +0700

    fix vercel handler

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,8 @@
-import app from "../server/index";
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import app from "../server/index";
 
+// Handler untuk menjalankan Express sebagai serverless function
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  // Express menerima req dan res langsung
   return app(req, res);
 }
